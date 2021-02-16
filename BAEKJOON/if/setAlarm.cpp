6b109aabe7317681418318d54a497fac905fe5c3
Yoonoh <xkakak142@naver.com>
2021-02-16 20:30:16 +0900

    if question

---
+++ BAEKJOON/if/setAlarm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int H, M;
+    cin >> H >> M;
+    if(M >= 45) cout << H << " " << M - 45;
+    else {
+        H == 0 ? cout << 23 << " " : cout << --H << " ";
+        cout << M + 15;
+    }
+        
+    return 0;
+}
