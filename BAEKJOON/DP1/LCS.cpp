cce7f2747e73433f2551c6936f8d0620c007a0f7
Yoonoh <xkakak142@naver.com>
2021-02-22 00:59:15 +0900

    LCS

---
+++ BAEKJOON/DP1/LCS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <string>
+using namespace std;
+
+string a,b;
+int table[1002][1002];
+
+int solve(){
+    for(int i = 1; i <= a.size(); i++){
+        for(int j = 1; j <= b.size(); j++){
+            if(a[i-1] == b[j-1]) table[i][j] = table[i-1][j-1] + 1;
+            else table[i][j] = max(table[i-1][j], table[i][j-1]);
+        }
+    }
+    return table[a.size()][b.size()];
+}
+
+int main(){
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    cin >> a >> b;
+    memset(table, 0, sizeof(table));
+    cout << solve();
+    return 0;
+}
--- BAEKJOON/DP1/electricWire.cpp
+++ BAEKJOON/DP1/electricWire.cpp
@@ -5,24 +5,18 @@ using namespace std;
 
 int N, range = 0, fmax = -1, rmax = -1, M;
 int wire[501]; // [a] = b  -> a,b ¿¬°á
-int fdp[501];
-int rdp[501];
+int dp[501];
 
 int solve(){
     for(int i = 1; i <= range; i++){
-        int maxPos = 0, maxRpos = 0;
-        for(int j = 1; j < i; j++){
+        int maxPos = 0;
+        for(int j = 1; j < i; j++)
             if((wire[j] != -1) && (wire[i] != -1) && (wire[j] < wire[i])) maxPos = max(maxPos, fdp[j]);
-            if((wire[range - j + 1] != -1) && (wire[range - i + 1] != -1) && (wire[range - j + 1] < wire[range - i + 1])) maxRpos = max(maxRpos, rdp[j]);
-        }
-        fdp[i] = maxPos + 1;
-        rdp[i] = maxRpos + 1;
+        dp[i] = maxPos + 1;
     }
-    for(int i = 1; i <= range; i++){
-        fmax = max(fmax, fdp[i]);
-        rmax = max(rmax, rdp[i]);
-    }    
-    M = max(fmax, rmax);    
+    for(int i = 1; i <= range; i++)
+        fmax = max(fmax, dp[i]);
+    M = max(M, fmax);
     return N - M;
 }
 
