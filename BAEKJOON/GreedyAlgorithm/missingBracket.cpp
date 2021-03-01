7352c1bc84bf53341840f28b688090e7be92930c
Yoonoh <xkakak142@naver.com>
2021-03-01 22:57:30 +0900

    missingBracket

---
+++ BAEKJOON/GreedyAlgorithm/missingBracket.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <algorithm>
+#include <string.h>
+#include <string>
+
+using namespace std;
+int main(){
+    cin.tie(NULL); cin.sync_with_stdio(false);
+    string s, temp;
+    cin >> s;
+    int idx = 0, num = 0, sum = 0;
+    bool state = false;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '+' || s[i] == '-'){
+            temp = s.substr(idx, i - idx); 
+            idx = i + 1; // 기호 다음칸 iterate
+            num = stoi(temp);
+            sum += state ? -num : num;
+            if(s[i] == '-') state = true; // 첫 숫자 skip
+        }
+    }
+    // 마지막 숫자
+    temp = s.substr(idx, s.size() - idx);
+    num = stoi(temp);
+    sum += state ? -num : num;
+    cout << sum;
+    return 0;
+}
