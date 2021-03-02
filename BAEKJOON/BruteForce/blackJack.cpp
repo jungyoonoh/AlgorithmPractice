024f1d47bd7198b1050802b1003560285d4106cb
Yoonoh <xkakak142@naver.com>
2021-03-02 12:29:51 +0900

    blackJack

---
+++ BAEKJOON/BruteForce/blackJack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+vector<int> card;
+int N, M, J = -1;
+// 전체 집합, 추출 표본, 뽑아야 할 갯수, 추출 표본의 현재 개수, 원소 iterator
+void Combination(vector<int> arr, vector<int> comb, int r, int index, int depth){
+    if (r == 0) {
+        int sum = 0;
+        for(int i = 0; i < comb.size(); i++)
+            sum += comb[i];
+        if(sum <= M) J = max(J,sum);
+    }
+    else if (depth == arr.size())  // depth == n
+        return;
+    else {
+        comb[index] = arr[depth];
+        Combination(arr, comb, r - 1, index + 1, depth + 1); // 현재 depth의 원소를 뽑기
+        Combination(arr, comb, r, index, depth + 1); // 현재 depth의 원소를 안뽑기
+    }
+}
+
+int main(){
+    cin.tie(NULL); cin.sync_with_stdio(false);
+    cin >> N >> M;
+    int r = 3,a;
+    vector<int> comb(r);
+    for(int i = 0; i < N; i++){
+        cin >> a;
+        card.push_back(a);
+    }
+    
+    Combination(card,comb,r,0,0);
+    cout << J;
+    return 0;
+}
--- BAEKJOON/GreedyAlgorithm/gasStation.cpp
+++ BAEKJOON/GreedyAlgorithm/gasStation.cpp
@@ -8,8 +8,10 @@ int oil[100000];
 // 기름 싼곳 찾아서 거기서 다음 싼곳 나올때까지 충전하면 가장 이득
 int main(){
     cin.tie(NULL); cin.sync_with_stdio(false);
-    // ?? int는 21억이 최대인데 10억 넣으면 오류난다고 백준이 채점함 
-    // 앵간하면 큰 자료형으로 때려박는게 이득
+    // 오버플로우가 나는 시점 == 곱해진 그 순간
+    // long long = int x int (x) -> overflow
+    // long long = long long x int (o)
+    // long long = long long x long long (o)
     long long n, min = 1000000001; 
     cin >> n;
     long long minimumCost = 0;   
