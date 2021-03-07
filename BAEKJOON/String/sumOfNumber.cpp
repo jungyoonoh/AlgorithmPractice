5bfd9f0b50a60116bea055d9db697973710a7ba5
Yoonoh <xkakak142@naver.com>
2021-03-08 01:25:50 +0900

    sumOfNumber

---
+++ BAEKJOON/String/sumOfNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    long long n,sum = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++)
+        sum += s.at(i) - '0';
+    cout << sum;    
+    return 0;
+}
