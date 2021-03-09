756e712fca69b0978c41151aba94899dcc6dfa2a
Yoonoh <xkakak142@naver.com>
2021-03-09 23:14:01 +0900

    multiply

---
+++ BAEKJOON/DivideAndConquer/multiply.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// A를 B번 곱한 숫자를 C로 나눠야함
+long long A,B,C;
+long long solution(long long a, long long b){
+    if(b == 0) return 1;
+    long long temp = solution(a, b / 2);
+    if(b % 2 == 0) return (temp * temp) % C;
+    if(b % 2 == 1) return (((temp * temp) % C) * A) % C;
+}
+
+int main(){
+    cin >> A >> B >> C;
+    cout << solution(A % C, B);
+    return 0;
+}
