ee18801cd958af6df9f07ee705e14d7acd7a30bc
Yoonoh <xkakak142@naver.com>
2021-03-15 02:34:48 +0900

    filip

---
+++ BAEKJOON/String/filip.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    int c = stoi(a);
+    int d = stoi(b);
+    c > d ? cout << c : cout << d;
+    return 0;
+}
