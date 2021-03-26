03c68d300fde57625a77e10a0bd73f4874ff34c6
Yoonoh <xkakak142@naver.com>
2021-03-26 15:39:49 +0900

    ACMHotel

---
+++ BAEKJOON/Mathmatics/ACMHotel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL); cin.sync_with_stdio(false);
+    int T,W,N,H;
+    cin >> T;
+    while(T--){
+        cin >> H >> W >> N;
+        if(N % H == 0)
+            N / H < 10 ? cout << H << "0" << N/H << '\n': cout << H << N/H << '\n';
+        else
+            N / H < 9 ? cout << N % H << "0" << N/H + 1 << '\n' : cout << N % H << N/H + 1 << '\n';
+    }
+    return 0; 
+}
