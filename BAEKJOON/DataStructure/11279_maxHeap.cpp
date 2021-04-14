fe95dae2bcbc05601bdb60b348e4d759b60e233c
Yoonoh <xkakak142@naver.com>
2021-04-14 22:47:35 +0900

    11279_maxHeap

---
+++ BAEKJOON/DataStructure/11279_maxHeap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int N,x;
+int main(){
+    cin.tie(NULL); cin.sync_with_stdio(false);
+    cin >> N;
+    priority_queue<int, vector<int>, less<int>> pq;
+    for(int i = 0; i < N; i++){
+        cin >> x;
+        if(x != 0)
+            pq.push(x);
+        else if(pq.size() == 0) cout << 0 << '\n';
+        else {
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+    }
+    return 0;
+}
