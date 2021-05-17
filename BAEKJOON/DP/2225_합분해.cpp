4e60a31fae4487d09d946e4ab7ee4112d2dfa31f
Yoonoh <xkakak142@naver.com>
2021-05-17 16:33:58 +0900

    2225_합분해

--- BAEKJOON/DP/2225_합분해.cpp
+++ BAEKJOON/DP/2225_합분해.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-
+#include <string.h>
+#define MOD 1000000000
 using namespace std;
 
-int N, K, C = 0;
+long long N, K, dp[201][201];
 
+long long solve(int n, int k){
+    if(n == 0 || k == 1) return dp[n][k] = 1;
+    if(dp[n][k] != 0) return dp[n][k];
 
-void solve(int idx, int cnt, int sum){
-    if(cnt == K){
-        if(sum == N){ C++; C %= 1000000000; return; }
-        else return;
-    }
-    for(int i = idx; i <= N; i++)
-        solve(i, cnt + 1, sum + i);
+    for(int i = 0; i <= n; i++)
+        dp[n][k] += solve(i, k-1) % MOD;
+    return dp[n][k] % MOD;
 }
 
 
 int main(){
     cin >> N >> K;
-    solve(0,0,0);    
-    cout << C;
+    memset(dp,0,sizeof(dp));
+    cout << solve(N,K);
 }
