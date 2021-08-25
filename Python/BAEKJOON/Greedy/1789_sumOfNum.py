43acd9a49a0a9ea8fe4fbea2927f85436370eb70
Yoonoh <xkakak142@naver.com>
2021-08-25 21:34:19 +0900

    solved 1789

---
+++ Python/BAEKJOON/Greedy/1789_sumOfNum.py
@@ -0,0 +1,18 @@
+# 1789번 수들의 합 (Greedy) 
+# https://www.acmicpc.net/problem/2720
+
+S = int(input())
+start = 1
+end = 4294967295
+sum = 0
+ans = -1
+while start <= end:
+    mid = (start + end) // 2
+    sum = (mid + 1) * mid // 2
+    if sum <= S:
+        start = mid + 1
+        ans = max(ans, mid)
+    else: 
+        end = mid - 1
+
+print(ans)
