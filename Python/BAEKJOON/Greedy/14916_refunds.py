dd9b19ee148585b5de006f7c6d38f279766d0086
Yoonoh <xkakak142@naver.com>
2021-08-27 17:38:35 +0900

    solved 14916

---
+++ Python/BAEKJOON/Greedy/14916_refunds.py
@@ -0,0 +1,14 @@
+# 1543번 거스름돈 (Greedy) 
+# https://www.acmicpc.net/problem/14916
+# 기본 거스름돈은 단위가 배수관계, 이 문제는 배수관계가 아닌 경우
+
+N = int(input())
+
+if N % 5 == 0:
+    print(N // 5)
+elif N == 1 or N == 3:
+    print(-1)
+elif N % 5 == 2 or N % 5 == 4:
+    print(N // 5 + ((N % 5) // 2))
+elif N % 5 == 1 or N % 5 == 3:
+    print((N - 5) // 5 + (((N - 5) % 5) + 5) // 2)
--- Python/BAEKJOON/Greedy/1543_searchDocs.py
+++ Python/BAEKJOON/Greedy/1543_searchDocs.py
@@ -1,11 +1,6 @@
 # 1543번 문서 검색 (Greedy) 
 # https://www.acmicpc.net/problem/1543
 
-import sys
-
 str = input()
 find = input()
-
-print(str)
-print(find)
 print(str.count(find))
