c3508f007ff4925b952cea6a50da5fd8e527fc03
Yoonoh <xkakak142@naver.com>
2021-09-03 13:08:57 +0900

    solved 9237

---
+++ Python/BAEKJOON/Greedy/9237_inviteHeadofVillage.py
@@ -0,0 +1,14 @@
+# 9237번 이장님 초대 (Greedy) 
+# https://www.acmicpc.net/problem/9237
+
+import sys
+N = int(input())
+
+data = list(map(int, sys.stdin.readline().split()))
+
+data.sort(reverse=True)
+
+for i in range(2, len(data) + 2):
+    data[i-2] += i
+
+print(max(data))
