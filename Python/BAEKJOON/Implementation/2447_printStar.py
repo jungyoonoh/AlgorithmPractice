d2aa6c23793d699b455e48de2207eb5cbc91803e
Yoonoh <xkakak142@naver.com>
2021-10-11 23:11:31 +0900

    solved 2447

---
+++ Python/BAEKJOON/Implementation/2447_printStar.py
@@ -0,0 +1,24 @@
+# 2447번 별찍기 10 (Implementation) 
+# https://www.acmicpc.net/problem/2447
+
+def drawStar(board, n, y, x):
+    if n == 1:
+        board[y][x] = '*'
+        return
+
+    nextN = n // 3
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            drawStar(board, nextN, y + i * nextN, x + j * nextN)
+    
+
+N = int(input())
+
+board = [[' ' for _ in range(N)] for _ in range(N)]
+
+drawStar(board, N, 0, 0)
+
+for row in board:
+    print(''.join(row))
